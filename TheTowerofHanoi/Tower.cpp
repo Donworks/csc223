a7c7a20e5e4f21b218a5ddf1b7b9d816b56f522d
Donworks <s7.3redseatsamg777@gmail.com>
2025-03-04 07:54:33 -0500

    These are fixed by switching one of the onboard functions to stack based on turners recommendation

---
+++ TheTowerofHanoi/Tower.cpp
@@ -0,0 +1,25 @@
+// C++ recursive function to solve tower of hanoi problem
+//geeks for geeks provided the code
+#include <stack>
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+    if (n == 0) {
+        return;
+    }
+    towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+    cout << "Move disk" << n << "from rod " << from_rod << "to rod" << to_rod << endl;
+    towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+}
+
+// Main code
+int main(){
+
+    int N = 3;
+
+    //A, B, C are the names of the rods
+    towerOfHanoi(N, 'A', 'C', 'B');
+    return 0;
+}
